621433861b5897232e7cf3cd4cda2a0a5c582665
uzum <a.uzumcuoglu@gmail.com>
2018-06-02 16:51:41 +0000

    remove cors headers as it's no longer necessary with static file serving

--- load_generator/server.py
+++ load_generator/server.py
@@ -7,40 +7,28 @@ load_generator = Blueprint('load_generator', __name__)
 
 @load_generator.route("/configuration", methods=['GET'])
 def getConfiguration():
-    response = jsonify(lg.getConfiguration())
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(lg.getConfiguration())
 
 @load_generator.route("/rrh/create", methods=['POST'])
 def addRRH():
-    response = jsonify(lg.addRRH(float(request.args.get('rate'))))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(lg.addRRH(float(request.args.get('rate'))))
 
 @load_generator.route("/rrh/<id>/remove", methods=['POST'])
 def removeRRH(id):
     lg.removeRRH(int(id))
-    response = jsonify({ 'success': True })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify({ 'success': True })
 
 @load_generator.route("/rrh/<id>/set-arrival-rate", methods=['POST'])
 def setArrivalRate(id):
-    response = jsonify(lg.setArrivalRate(int(id), float(request.args.get('rate'))))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(lg.setArrivalRate(int(id), float(request.args.get('rate'))))
 
 @load_generator.route("/rrh/<id>/add-connection", methods=['POST'])
 def addConnection(id):
-    response = jsonify(lg.addConnection(int(id), int(request.args.get('amount'))))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(lg.addConnection(int(id), int(request.args.get('amount'))))
 
 @load_generator.route("/rrh/<id>/remove-connection", methods=['POST'])
 def removeConnection(id):
-    response = jsonify(lg.removeConnection(int(id), int(request.args.get('amount'))))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(lg.removeConnection(int(id), int(request.args.get('amount'))))
 
 class LGServer():
     def __init__(self, app):
