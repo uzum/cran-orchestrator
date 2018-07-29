4464a20aa9731c563908818111ed68b750209347
uzum <a.uzumcuoglu@gmail.com>
2018-07-29 21:36:51 +0300

    allow POST requests to be proxied through control-panel

--- control_panel/server.py
+++ control_panel/server.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, Blueprint, Response, stream_with_context
+from flask import Flask, Blueprint, Response, stream_with_context, request
 
 from .config import *
 
@@ -11,23 +11,34 @@ class CPServer():
         app.register_blueprint(control_panel, url_prefix='')
         app.run(host='0.0.0.0', port=CP_SERVICE_PORT)
 
-    @control_panel.route("/openstack-client/<path:url>")
+    @control_panel.route("/openstack-client/<path:url>", methods=["GET", "POST"])
     def OCProxy(url):
-        req = requests.get(OC_SERVICE_URL + '/openstack-client/' + url, stream = True)
-        return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+        if (request.method == 'GET'):
+            req = requests.get(OC_SERVICE_URL + '/openstack-client/' + url, stream=True, params=request.args)
+        else:
+            req = requests.post(OC_SERVICE_URL + '/openstack-client/' + url, stream=True, params=request.args)
+        return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
 
-    @control_panel.route("/resource-mapper/<path:url>")
+    @control_panel.route("/resource-mapper/<path:url>", methods=["GET", "POST"])
     def RMProxy(url):
-        req = requests.get(RM_SERVICE_URL + '/resource-mapper/' + url, stream = True)
-        return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+        if (request.method == 'GET'):
+            req = requests.get(RM_SERVICE_URL + '/resource-mapper/' + url, stream=True, params=request.args)
+        else:
+            req = requests.post(RM_SERVICE_URL + '/resource-mapper/' + url, stream=True, params=request.args)
+        return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
 
-    @control_panel.route("/load-generator/<path:url>")
+    @control_panel.route("/load-generator/<path:url>", methods=["GET", "POST"])
     def LGProxy(url):
-        print("here")
-        req = requests.get(LG_SERVICE_URL + '/load-generator/' + url, stream = True)
-        return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+        if (request.method == 'GET'):
+            req = requests.get(LG_SERVICE_URL + '/load-generator/' + url, stream=True, params=request.args)
+        else:
+            req = requests.post(LG_SERVICE_URL + '/load-generator/' + url, stream=True, params=request.args)
+        return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
 
-    @control_panel.route("/log-collector/<path:url>")
+    @control_panel.route("/log-collector/<path:url>", methods=["GET", "POST"])
     def LCProxy(url):
-        req = requests.get(LC_SERVICE_URL + '/log-collector/' + url, stream = True)
-        return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+        if (request.method == 'GET'):
+            req = requests.get(LC_SERVICE_URL + '/log-collector/' + url, stream=True, params=request.args)
+        else:
+            req = requests.post(LC_SERVICE_URL + '/log-collector/' + url, stream=True, params=request.args)
+        return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
