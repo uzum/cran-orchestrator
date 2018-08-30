462db944ad3727ce748430eac3fcbe0bf8232023
uzum <a.uzumcuoglu@gmail.com>
2018-08-30 13:25:36 +0300

    test: implement a non-blocking timer in udp server for status reporting

--- test/udp/server.py
+++ test/udp/server.py
@@ -1,14 +1,25 @@
 import socket
 import argparse
+from threading import Timer
+
+REPORT_INTERVAL = 10.0
 
 parser = argparse.ArgumentParser()
 parser.add_argument("port")
 args = parser.parse_args()
 
+reportTimer = None
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', int(args.port)))
 
+def report():
+    print('reporting to log-collector')
+    timer = Timer(REPORT_INTERVAL, report)
+    timer.start()
+
 print('listening to ' + args.port)
+report()
+
 while True:
     (message, address) = server_socket.recvfrom(1024)
     print('received message from ' + str(address))
