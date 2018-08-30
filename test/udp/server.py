1c4adc639260977c0ac90fe7f1290a4072ab004d
uzum <a.uzumcuoglu@gmail.com>
2018-08-30 15:34:29 +0300

    test: include cpu and memory utilization in periodical reports coming from udp servers

--- test/udp/server.py
+++ test/udp/server.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import time
 import argparse
 from threading import Timer
 
@@ -9,11 +11,21 @@ parser.add_argument("port")
 args = parser.parse_args()
 
 reportTimer = None
+packetCount = 0
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', int(args.port)))
 
 def report():
-    print('reporting to log-collector')
+    cpuUtilization = round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()))
+    total, used, free = list(map(int, os.popen('free -t -m').readlines()[-1].split()[1:]))
+    memoryUtilization = round(float(used)/total, 2) * 100
+    payload = {
+        'timestamp': int(time.time()),
+        'packetCount': packetCount,
+        'cpuUtilization': cpuUtilization,
+        'memoryUtilization': memoryUtilization
+    }
+    print(payload)
     timer = Timer(REPORT_INTERVAL, report)
     timer.start()
 
@@ -22,5 +34,6 @@ report()
 
 while True:
     (message, address) = server_socket.recvfrom(1024)
+    packetCount += 1
     print('received message from ' + str(address))
     print(message.decode('utf-8'))
