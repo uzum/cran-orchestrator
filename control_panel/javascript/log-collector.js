b86e82f2f3cac174d2f66c65ebdbc37e518d46f5
uzum <a.uzumcuoglu@gmail.com>
2018-09-03 22:57:52 +0300

    control-panel: differentiate log-collector entries with respect to source

--- control_panel/javascript/log-collector.js
+++ control_panel/javascript/log-collector.js
@@ -1,14 +1,52 @@
+const classes = ['primary', 'secondary', 'dark', 'light', 'info', 'warning', 'success', 'danger'];
+const assignedClasses = new Map();
+
 Vue.component('history-entry', {
   props: ['entry'],
   template: `
-    <div class="history-item">{{ JSON.stringify(this.entry) }}</div>
-  `
-})
+    <div class="history-item row">
+      <div class="col-md-2">
+        <span class="badge badge-light">{{ entry.timestamp }}</span>
+      </div>
+      <div class="col-md-2">
+        <span v-bind:class="sourceClass">{{ entry.source }}</span>
+      </div>
+      <div class="col-md-6">
+        <code>{{ payload }}</code>
+      </div>
+    </div>
+  `,
+  computed: {
+    payload: function(){
+      const object = {};
+      Object.keys(this.entry).forEach(key => {
+        if (key !== 'timestamp' && key !== 'source') {
+          object[key] = this.entry[key];
+        }
+      });
+      return object;
+    },
+    sourceClass: function(){
+      if (assignedClasses.has(this.entry.source)) {
+        return assignedClasses.get(this.entry.source);
+      } else {
+        const assignedClass = `badge badge-${classes[Math.floor(Math.random() * classes.length)]}`;
+        assignedClasses.set(this.entry.source, assignedClass);
+        return assignedClass;
+      }
+    }
+  }
+});
 
 Vue.component('log-history', {
   props: ['history'],
   template: `
     <div class="history-container">
+      <div class="row">
+        <div class="col-md-2"><b>Timestamp</b></div>
+        <div class="col-md-2"><b>Source</b></div>
+        <div class="col-md-2"><b>Payload</b></div>
+      </div>
       <history-entry
         v-for="entry in history"
         v-bind:key="entry.timestamp"
@@ -34,14 +72,14 @@ const LC = new Vue({
   },
   methods: {
     peek: function(){
-      console.log('peeking');
       axios.get(`${LCServerURL}/peek?index=${this.lastSeenIndex}`)
         .then((response) => {
           response.data.forEach(entry => {
             this.history.push(entry);
-            if (this.history.length > HISTORY_CAPACITY) this.history.unshift();
+            if (this.history.length > HISTORY_CAPACITY) this.history.shift();
           });
           setTimeout(this.peek, PEEK_INTERVAL);
+          this.lastSeenIndex += response.data.length;
         })
         .catch((error) => {
           console.log(error);
