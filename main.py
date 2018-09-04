82ea9944b9b197aa69edd5134dc8937f74680f96
uzum <anil.uzumcuoglu@arcelik.com>
2018-09-04 10:12:20 +0300

    import cran modules only when they are necessary

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from optparse import OptionParser
 
-# from load_generator import LGServer
-# from resource_mapper import RMServer
-# from control_panel import CPServer
-# from openstack_client import OCServer
-from log_collector import LCServer
-
 if __name__ == "__main__":
     parser = OptionParser()
 
@@ -18,17 +12,22 @@ if __name__ == "__main__":
     (options, args) = parser.parse_args()
 
     if (options.loadgenerator):
+        from load_generator import LGServer
         print('load generator activated')
         lgServer = LGServer()
     if (options.resourcemapper):
+        from resource_mapper import RMServer
         print('resource mapper activated')
         rmServer = RMServer()
     if (options.controlpanel):
+        from control_panel import CPServer
         print('control panel activated')
         cpServer = CPServer()
     if (options.logcollector):
+        from log_collector import LCServer
         print('log collector activated')
         lcServer = LCServer()
     if (options.openstackclient):
+        from openstack_client import OCServer
         print('openstack client activated')
         ocServer = OCServer()
