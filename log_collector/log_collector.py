a4057acba9054c4f7d7e0f592b901d7e747327ae
uzum <a.uzumcuoglu@gmail.com>
2018-10-20 19:07:54 +0300

    log-collector: distinguish between creation and migration triggered watch requests

--- log_collector/log_collector.py
+++ log_collector/log_collector.py
@@ -17,15 +17,21 @@ class RMAPI():
         }
         return requests.post(self.pathPrefix + path, data=data, headers=headers)
 
-    def notify(self, address):
+    def notifyMigration(self, address):
         return requestPost('/resource-mapper/topology/bbu-migration?address=' + address)
+
+    def notifyCreation(self, address):
+        return requestPost('/resource-mapper/topology/bbu-creation?address=' + address)
 
 class LogCollector():
     CAPACITY = 1000
 
     def __init__(self):
         self.history = []
-        self.watchlist = []
+        self.watchlist = {
+            'migration': [],
+            'creation': []
+        }
         self.RMAPI = RMAPI()
 
     def findLastSeen(self, timestamp):
@@ -38,10 +44,15 @@ class LogCollector():
 
     def append(self, entry):
         self.history.insert(0, entry)
-        for bbuAddress in self.watchlist:
+        for bbuAddress in self.watchlist['migration']:
             if (bbuAddress == entry.address):
-                self.RMAPI.notify(entry.address)
-                self.watchlist.remove(bbuAddress)
+                self.RMAPI.notifyMigration(entry.address)
+                self.watchlist['migration'].remove(bbuAddress)
+
+        for bbuAddress in self.watchlist['creation']:
+            if (bbuAddress == entry.address):
+                self.RMAPI.notifyCreation(entry.address)
+                self.watchlist['creation'].remove(bbuAddress)
 
         if (len(self.history) > LogCollector.CAPACITY):
             self.history.pop()
@@ -50,5 +61,5 @@ class LogCollector():
     def peek(self, timestamp = 0):
         return self.history[0:self.findLastSeen(timestamp)]
 
-    def watch(self, address):
-        self.watchlist.append(address)
+    def watch(self, list, address):
+        self.watchlist[list].append(address)
